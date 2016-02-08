c8e364dd1cef63348dc329aae988a8f044f85f90
Siddharth Jinagouda <siddharthjinagouda@gmail.com>
2016-02-08 16:44:51 +0530

    image_url is not mandatory

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ActiveRecord::Base
 
 	#..
 
-	validates :title, :description, :image_url, presence: true
+	validates :title, :description, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	#
 	validates :title, uniqueness: true
